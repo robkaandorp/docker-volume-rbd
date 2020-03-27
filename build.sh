c2018574e40eaaeeb2b7266f6a53fd377ad55a75
Rob Kaandorp <rob@di.nl>
2020-03-27 14:09:50 +0100

    Build plugin in clean folder

--- build.sh
+++ build.sh
@@ -2,17 +2,18 @@
 
 docker plugin disable robkaandorp/rbd:v15.2 -f
 docker plugin rm robkaandorp/rbd:v15.2 -f
-rm -rf rootfs
+rm -rf plugin
 
 git pull
 docker build . -t robkaandorp/rbd:v15.2
 
 id=$(docker create robkaandorp/rbd:v15.2 true)
-mkdir rootfs
-docker export "$id" | sudo tar -x -C rootfs
+mkdir -p plugin/rootfs
+cp config.json plugin/
+docker export "$id" | sudo tar -x -C plugin/rootfs
 docker rm -vf "$id"
 docker rmi robkaandorp/rbd:v15.2
 
-docker plugin create robkaandorp/rbd:v15.2 .
+docker plugin create robkaandorp/rbd:v15.2 plugin/
 docker plugin enable robkaandorp/rbd:v15.2
 docker plugin ls
