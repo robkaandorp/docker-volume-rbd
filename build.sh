299ecc2125fa43277179511b5bc995e96c92c856
Ashley <webash@users.noreply.github.com>
2024-12-29 15:50:24 +0000

    Update build.sh for version to be more easily configurable

--- build.sh
+++ build.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-docker plugin disable robkaandorp/rbd:v17.2 -f
-docker plugin rm robkaandorp/rbd:v17.2 -f
+version=19.2
+pluginname=robkaandorp/rbd
+pluginnametagged=$pluginname:$version
+echo $pluginnametagged
+
+docker plugin disable $pluginnametagged -f
+docker plugin rm $pluginnametagged -f
 rm -rf plugin
 
 git pull
-docker build . -t robkaandorp/rbd:v17.2
+docker build . -t $pluginnametagged
 
-id=$(docker create robkaandorp/rbd:v17.2 true)
+id=$(docker create $pluginnametagged true)
 mkdir -p plugin/rootfs
 cp config.json plugin/
 docker export "$id" | sudo tar -x -C plugin/rootfs
 docker rm -vf "$id"
-docker rmi robkaandorp/rbd:v17.2
+docker rmi $pluginnametagged
 
-docker plugin create robkaandorp/rbd:v17.2 plugin/
-docker plugin enable robkaandorp/rbd:v17.2
+docker plugin create $pluginnametagged plugin/
+docker plugin enable $pluginnametagged
 docker plugin ls
